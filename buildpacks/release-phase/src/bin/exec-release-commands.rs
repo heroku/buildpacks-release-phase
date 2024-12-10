eed83b0729cc1ba9e0f10db2475eb2caefda4aea
Angel Campbell <angel.campbell@salesforce.com>
2024-12-10 12:33:21 -0800

    stream logs to stdio; inject PATH

--- buildpacks/release-phase/src/bin/exec-release-commands.rs
+++ buildpacks/release-phase/src/bin/exec-release-commands.rs
@@ -1,7 +1,11 @@
 // Required due to: https://github.com/rust-lang/rust/issues/95513
 #![allow(unused_crate_dependencies)]
 
-use std::{env, path::Path, process::Command};
+use std::{
+    env,
+    path::Path,
+    process::{Command, Stdio},
+};
 
 use release_commands::read_commands_config;
 
@@ -32,54 +36,58 @@ fn exec_release_sequence(commands_toml_path: &Path) -> Result<(), release_comman
     if let Some(release_build_config) = config.release_build {
         eprintln!("release-phase executing release-build command: {release_build_config}");
         let mut cmd = Command::new(release_build_config.command);
-        release_build_config.args.clone().map(|v| cmd.args(v));
-        let output = cmd
-            .output()
-            .map_err(release_commands::Error::ReleaseCommandExecError)?;
-        let log = tidy_logs(
-            String::from_utf8_lossy(&output.stderr).as_ref(),
-            String::from_utf8_lossy(&output.stdout).as_ref(),
-        );
-        if output.status.code() != Some(0) {
-            return Err(release_commands::Error::ReleaseCommandExitedError(log));
+        if let Some(args) = release_build_config.args {
+            cmd.args(args);
         }
-        print!("{log}");
+        if let Ok(path) = env::var("PATH") {
+            cmd.env("PATH", path);
+        }
+        let status = cmd
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .status()
+            .map_err(release_commands::Error::ReleaseCommandExecError)?;
+
+        match status.code() {
+            None => (),
+            Some(0) => (),
+            Some(code) => {
+                return Err(release_commands::Error::ReleaseCommandExitedError(format!(
+                    "command exited with status code {}",
+                    code
+                )))
+            }
+        }
     };
 
     if let Some(release_config) = config.release {
         for config in &release_config {
             eprintln!("release-phase executing release command: {config}");
             let mut cmd = Command::new(&config.command);
-            config.args.clone().map(|v| cmd.args(v));
-            let output = cmd
-                .output()
-                .map_err(release_commands::Error::ReleaseCommandExecError)?;
-            let log = tidy_logs(
-                String::from_utf8_lossy(&output.stderr).as_ref(),
-                String::from_utf8_lossy(&output.stdout).as_ref(),
-            );
-            if output.status.code() != Some(0) {
-                return Err(release_commands::Error::ReleaseCommandExitedError(log));
+            if let Some(args) = &config.args {
+                cmd.args(args.clone());
             }
-            print!("{log}");
+
+            let status = cmd
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .status()
+                .map_err(release_commands::Error::ReleaseCommandExecError)?;
+
+            match status.code() {
+                None => (),
+                Some(0) => (),
+                Some(code) => {
+                    return Err(release_commands::Error::ReleaseCommandExitedError(format!(
+                        "command exited with status code {}",
+                        code
+                    )))
+                }
+            }
         }
     };
 
     Ok(())
-}
-
-fn tidy_logs(stderr: &str, stdout: &str) -> String {
-    let mut output = String::new();
-    if !stderr.is_empty() {
-        output = output + "stderr:\n" + stderr;
-    }
-    if !stderr.is_empty() && !stdout.is_empty() {
-        output += "\n";
-    }
-    if !stdout.is_empty() {
-        output = output + "stdout:\n" + stdout;
-    }
-    output
 }
 
 #[cfg(test)]
